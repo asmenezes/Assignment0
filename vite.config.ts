95ad64ebe1913e61ac1578a60648c070f718204d
asmenezes <asmenezes@mail.com>
2026-09-20 19:28:20 -0500

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+
+
+/** @type {import('@sveltejs/kit').Config} */
 
 export default defineConfig({
 	plugins: [
@@ -10,15 +14,18 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-
+			preprocess: vitePreprocess(),
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			//adapter: adapter()
 			adapter: adapter({
-				fallback: '404.html'//,
-				//pages: "build/"
+				fallback: '404.html',
+				pages: "build/"
 			}),
+			paths: {
+				base: '/Assignment0'
+			}
 		})
 	]
 });
